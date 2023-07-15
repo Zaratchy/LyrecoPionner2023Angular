318ce959a16b5cf11704dadab75c089ceea34fde
Josic <libert.josic@gmail.com>
2023-07-15 21:50:12 +0200

    feature/software: add component software add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SoftwareComponent} from "./Contenairs/software/software.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-software', component: SoftwareComponent },
+  { path: '',   redirectTo: '/app-home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SoftwareComponent } from './Contenairs/software/software.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SoftwareComponent
   ],
   imports: [
     BrowserModule,
