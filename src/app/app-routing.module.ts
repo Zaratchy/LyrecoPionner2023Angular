bb9f0dfb32612c4d4bd30ab017642f194cf7183f
Josic <libert.josic@gmail.com>
2023-07-18 21:43:19 +0200

    feature/layoutNav: add component navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-navbar', component: NavbarComponent },
+  { path: '',   redirectTo: '/app-home', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
