9c4e5f0328a2210ce93b0bec047af397e7c8b076
Josic <libert.josic@gmail.com>
2023-07-19 21:12:56 +0200

    feature/layoutHeader: add header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 const routes: Routes = [
-  { path: 'app-navbar', component: NavbarComponent },
-  { path: '',   redirectTo: '/app-home', pathMatch: 'full' },
+  { path: 'header', component: HeaderComponent },
+  { path: 'navbar', component: NavbarComponent },
+ /* { path: '',   redirectTo: '/app-home', pathMatch: 'full' },*/
 
 ];
 
