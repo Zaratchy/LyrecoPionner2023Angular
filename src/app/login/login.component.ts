7a6a7b0d5a3cd4607746b9e2cb7fd549a9a6eafe
Josic <libert.josic@gmail.com>
2023-07-23 22:02:30 +0200

    feature/registerLogin: register good

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AlertService} from "../services/alert.service";
+import {AuthentificationService} from "../services/authentification.service";
+import {first} from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup | any;
+  loading = false;
+  submitted = false;
+  returnUrl: string | undefined;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authentificationService: AuthentificationService,
+    private alertService: AlertService
+  ) {
+    // redirect to home if already logged in
+    if (this.authentificationService.currentCustomerValue) {
+      this.router.navigate(['/']);
+    }
+  }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authentificationService.login(this.f['firstname'].value, this.f['password'].value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+
   }
 
 }
